bdbe49df99290056c07f5c184f6883afd268db71
dudouro <150480310+dudouro@users.noreply.github.com>
2023-11-10 17:08:00 -0300

    Add files via upload

---
+++ AulaPOO/src/main/java/com/mycompany/aulapoo/AulaPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aulapoo;
+
+/**
+ *
+ * @author eduar
+ */
+public class AulaPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
